9e44ccb4156653060dff5b5856cc5d6326deab0a
CTY250 <61401251+CTY250@users.noreply.github.com>
2021-06-10 00:15:18 -0400

    Update hellothere.java

--- hellothere.java
+++ hellothere.java
@@ -8,5 +8,8 @@ public class hellothere
      System.out.println("This code also works");
      System.out.println("Hey, you're right!");
      //Comments also work too, but that's pretty irrelevant
+     //Hey, you're irrelevant
+     //What do you mean, I'm irrelevant...we're all irrelevant
+     //Don't pull the big vast universe vs tiny comment argument on me
   }
 }
